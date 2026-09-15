6d325bb21289f5fd440746dd2950be4fbf1ea288
Maple_Miles <maplemilesalt@gmail.com>
2026-09-15 11:46:08 -0700

    Expand ARMv7 CPU state and condition support

--- src/cpu/armv7.h
+++ src/cpu/armv7.h
@@ -15,8 +15,11 @@ public:
 
     std::uint32_t reg(unsigned index) const;
     std::uint32_t pc() const { return regs_[15]; }
+    std::uint32_t cpsr() const { return cpsr_; }
 
 private:
+    bool condition_passed(std::uint32_t condition) const;
+    void set_nz(std::uint32_t result);
     void execute_arm(std::uint32_t instruction);
 
     Memory& memory_;
